fa4e173cb3612c764bd67ecadbd219f8bed1f09c
leaked32 <leaked32@outlook.com>
2026-08-30 14:49:29 +0900

    Update version information.

--- common/src/main/java/com/example/vitalrelics/common/platform/MyRuntimeUtils.java
+++ common/src/main/java/com/example/vitalrelics/common/platform/MyRuntimeUtils.java
@@ -6,28 +6,27 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MyRuntimeUtils {
-	List<Relic> gatherRelics(MyLivingEntity entity);
 
+	// Ray-cast
 	MyLivingEntity pointedLivingEntity(MyLivingEntity source, double range);
-
 	MyVec3 safeDestinationAlongLook(MyLivingEntity entity, double range);
 	MyVec3 safeHorizontalDestination(MyLivingEntity entity, double range);
-
 	List<MyLivingEntity> entitiesIntersectingMovement(MyLivingEntity entity, MyVec3 from, MyVec3 to, double inflate);
 
+	// HUD
 	void syncSpellHud(MyLivingEntity caster, String spellId, int cooldownTicks);
 	void clearSpellHud(MyLivingEntity caster);
 	void showSelectedSpell(MyLivingEntity caster, String spellId);
 	void showSpellCooldown(MyLivingEntity caster, String spellId, int remainingTicks);
 
+	// Utils
+	List<Relic> gatherRelics(MyLivingEntity entity);
+	boolean isEntityValid(UUID uuid);
 	void showCurseRequiresTarget(MyLivingEntity caster);
 	void summonVisualLightning(MyLivingEntity target);
 
-	boolean isEntityValid(UUID uuid);
-
-	enum EnchantmentFilter {ENCHANTMENT_BOOK_ONLY, ALL_ENCHANTED_ITEMS}
-
 	// Enchantments
+	enum EnchantmentFilter {ENCHANTMENT_BOOK_ONLY, ALL_ENCHANTED_ITEMS}
 	boolean upgradeFirstEnchantment(MyLivingEntity entity, int experienceCost, EnchantmentFilter option);
 	boolean removeCurse(MyLivingEntity entity, int experienceCost);
 	boolean resetRepairCost(MyLivingEntity entity, int experienceCost);
