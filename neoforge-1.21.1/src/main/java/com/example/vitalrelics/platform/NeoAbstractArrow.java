024a7bc6c83cb7f268fcb0876f24a52b39697d4f
November Room <leaked32@outlook.com>
2026-09-01 08:54:12 +0900

    Add NeoForge arrow adapter

---
+++ neoforge-1.21.1/src/main/java/com/example/vitalrelics/platform/NeoAbstractArrow.java
@@ -0,0 +1,45 @@
+package com.example.vitalrelics.platform;
+
+import com.example.vitalrelics.Utils;
+import com.example.vitalrelics.common.platform.MyAbstractArrow;
+import com.example.vitalrelics.common.platform.MyLivingEntity;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+
+public final class NeoAbstractArrow implements MyAbstractArrow {
+	private final AbstractArrow arrow;
+
+	public NeoAbstractArrow(final AbstractArrow arrow) {
+		this.arrow = arrow;
+	}
+
+	@Override
+	public double baseDamage() {
+		return arrow.getBaseDamage();
+	}
+
+	@Override
+	public void setBaseDamage(final double damage) {
+		arrow.setBaseDamage(damage);
+	}
+
+	@Override
+	public void scaleVelocity(final double multiplier) {
+		arrow.setDeltaMovement(arrow.getDeltaMovement().scale(multiplier));
+	}
+
+	@Override
+	public void retarget(
+			final MyLivingEntity newOwner,
+			final double speedMultiplier,
+			final double damageMultiplier,
+			final double minimumDamageFromAttack) {
+
+		Utils.retargetArrow(
+				arrow,
+				((NeoLivingEntity) newOwner).nativeEntity(),
+				speedMultiplier,
+				damageMultiplier,
+				minimumDamageFromAttack
+		);
+	}
+}
