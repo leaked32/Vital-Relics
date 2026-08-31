5191227f35e7ea24ba96155bedec60f8407e40b0
November Room <leaked32@outlook.com>
2026-08-31 15:38:14 +0900

    Remove obsolete synthetic damage source

--- common/src/main/java/com/example/vitalrelics/common/platform/MyExtraDamageSource.java
+++
@@ -1,8 +0,0 @@
-package com.example.vitalrelics.common.platform;
-
-public record MyExtraDamageSource(MyLivingEntity attacker) implements MyDamageSource {
-	@Override
-	public MyDamageKind kind() {
-		return MyDamageKind.EXTRA_DAMAGE;
-	}
-}
