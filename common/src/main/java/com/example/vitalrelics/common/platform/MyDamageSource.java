9a9c691b03e5cf9fb8eccb116a68ed2b457735fa
November Room <leaked32@outlook.com>
2026-08-31 15:06:07 +0900

    Preserve damage kind on MyDamageSource interface

--- common/src/main/java/com/example/vitalrelics/common/platform/MyDamageSource.java
+++ common/src/main/java/com/example/vitalrelics/common/platform/MyDamageSource.java
@@ -3,5 +3,14 @@ package com.example.vitalrelics.common.platform;
 public interface MyDamageSource {
 	MyLivingEntity attacker();
 
-	boolean isExtraDamage();
+	MyDamageKind kind();
+
+	default boolean isExtraDamage() {
+		return kind() == MyDamageKind.EXTRA_DAMAGE;
+	}
+
+	enum MyDamageKind {
+		EXTRA_DAMAGE,
+		OTHER
+	}
 }
