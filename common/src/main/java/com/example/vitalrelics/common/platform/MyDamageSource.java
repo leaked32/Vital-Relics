d7d3bc3d773da2f9971d68480e37430a0c75fdea
November Room <leaked32@outlook.com>
2026-08-31 15:38:07 +0900

    Represent native damage sources in common

--- common/src/main/java/com/example/vitalrelics/common/platform/MyDamageSource.java
+++ common/src/main/java/com/example/vitalrelics/common/platform/MyDamageSource.java
@@ -3,14 +3,5 @@ package com.example.vitalrelics.common.platform;
 public interface MyDamageSource {
 	MyLivingEntity attacker();
 
-	MyDamageKind kind();
-
-	default boolean isExtraDamage() {
-		return kind() == MyDamageKind.EXTRA_DAMAGE;
-	}
-
-	enum MyDamageKind {
-		EXTRA_DAMAGE,
-		OTHER
-	}
+	boolean isExtraDamage();
 }
