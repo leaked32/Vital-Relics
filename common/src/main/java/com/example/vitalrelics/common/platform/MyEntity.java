528683319efb0f6504ad0c808da3998cd41da94a
November Room <leaked32@outlook.com>
2026-09-01 19:50:47 +0900

    Support generic entity movement queries

--- common/src/main/java/com/example/vitalrelics/common/platform/MyEntity.java
+++ common/src/main/java/com/example/vitalrelics/common/platform/MyEntity.java
@@ -1,17 +1,22 @@
 package com.example.vitalrelics.common.platform;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MyEntity {
 	double x();
 	double y();
 	double z();
+	double height();
 
+	void moveTo(double x, double y, double z);
 	void setVelocity(double x, double y, double z);
 	void markMovementChanged();
 
 	boolean isLoaded();
 	boolean isClientSide();
+
+	List<MyEntity> entitiesInRange(double radius);
 
 	UUID uuid();
 }
