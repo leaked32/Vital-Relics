e899cee650c3a6e1a97bdee74ac03128c7a7fc6c
November Room <leaked32@outlook.com>
2026-09-01 18:59:27 +0900

    Make MyAbstractArrow extend MyEntity

--- common/src/main/java/com/example/vitalrelics/common/platform/MyAbstractArrow.java
+++ common/src/main/java/com/example/vitalrelics/common/platform/MyAbstractArrow.java
@@ -1,18 +1,11 @@
 package com.example.vitalrelics.common.platform;
 
-public interface MyAbstractArrow {
+public interface MyAbstractArrow extends MyEntity {
 	MyLivingEntity owner();
-
-	double x();
-	double y();
-	double z();
 
 	double velocityX();
 	double velocityY();
 	double velocityZ();
-
-	void setVelocity(double x, double y, double z);
-	void markMovementChanged();
 
 	double baseDamage();
 	void setBaseDamage(double damage);
